84641b227fcff6d60ce34f534af7d2453a701a84
ashtiv <ashishtiwari05032002@gmail.com>
2022-12-14 17:57:16 +0530

    some deletions

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
 import './App.css';
-import Header from './components/header';
 import RegistrationForm from './components/registrationForm'
-import { BrowserRouter as Router,Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Payment from './components/payment';
 
 function App() {
   return (
-    
+
     <div className="App">
-      {/* <Header /> */}
       <Router>
-           <Routes>
-                 <Route exact path='/' element={<RegistrationForm/>}></Route>
-                 <Route exact path='/payment' element={< Payment />}></Route>
-          </Routes>
-       </Router>
+        <Routes>
+          <Route exact path='/' element={<RegistrationForm />}></Route>
+          <Route exact path='/payment' element={< Payment />}></Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
